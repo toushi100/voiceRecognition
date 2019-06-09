f10defc6f2ac929ee6b5e5dc1358a7fe455a65ae
toushi <toushi2009@gmail.com>
2019-06-09 11:03:20 +0200

    fixed the .includes error

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const btn = document.querySelector('#talk');
 const content = document.querySelector('#content');
 const greetings = ['im good homeboi','why do you care','Fuck off'];
-const weather = ['it\'s fine why do you even care you dont even get out of you room','it\'s 55 a lovely weather for a tan'];
+const weather = ['it\'s fine why do you even care you dont even get out of you room','it\'s 55 degrees! a lovely weather for a tan'];
 const SpeechRecognitiion = window.SpeechRecognitiion || window.webkitSpeechRecognition;
 
 const recognition = new SpeechRecognitiion();
@@ -12,7 +12,7 @@ recognition.onstart = function (){
 
 recognition.onresult= function (event) {
    const current = event.resultIndex;
-   const transcript = event.results[current][0];
+   const transcript = event.results[current][0].transcript;
    content.textContent = transcript;
 
    readOutLoud(transcript);
@@ -20,21 +20,24 @@ recognition.onresult= function (event) {
 
 
 btn.addEventListener('click',()=>{
-    recognition.onstart();
+    recognition.start();
 });
 
 function readOutLoud(message){
 const speech = new SpeechSynthesisUtterance();
-
 if(message.includes('how are you')){
-    const finaltext = greetings[Math.floor(Math.random* greetings.length())];
+    const finaltext = greetings[Math.floor(Math.random() * greetings.length)];
     speech.text = finaltext;
+}else if(message.includes('weather')){
+    const finaltext = weather[Math.floor(Math.random() * weather.length)];
+    speech.text = finaltext;
+} else{
+    speech.text = message;
 }
+
 speech.volume=1;
-speech.rate=1;
+speech.rate=0.7;
 speech.pitch=1;
-
-
 
 window.speechSynthesis.speak(speech);
 }
